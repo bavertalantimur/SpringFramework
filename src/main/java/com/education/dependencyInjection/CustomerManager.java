425c69de7632259c2164f0e91598ce1f7591ee0c
baver <bawer.talantimur@gmail.com>
2022-10-26 13:21:18 +0300

    ıoc process

--- src/main/java/com/education/dependencyInjection/CustomerManager.java
+++ src/main/java/com/education/dependencyInjection/CustomerManager.java
@@ -2,13 +2,15 @@ package com.education.dependencyInjection;
 
 public class CustomerManager {
     private ICustomerDal customerDal;
+    int a;
     public CustomerManager(ICustomerDal customerDal) {
         this.customerDal = customerDal;
     }
 
     public void add(){
 
-        customerDal.add();;
+        customerDal.add();
+
     }
 
 
