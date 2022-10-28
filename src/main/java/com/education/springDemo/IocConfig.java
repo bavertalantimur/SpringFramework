e51cf2b41258fcf228c0481b05a7b85d2b360e8f
baver <bawer.talantimur@gmail.com>
2022-10-28 21:10:21 +0300

    switch to class config file

---
+++ src/main/java/com/education/springDemo/IocConfig.java
@@ -0,0 +1,10 @@
+package com.education.springDemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("com.education.springDemo")
+public class IocConfig {
+}
