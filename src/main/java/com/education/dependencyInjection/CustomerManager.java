00d4bd810b5cbdf93cf42633a7849e0c0cc4026c
baver <bawer.talantimur@gmail.com>
2022-10-28 12:24:17 +0300

    String injection process

--- src/main/java/com/education/dependencyInjection/CustomerManager.java
+++ src/main/java/com/education/dependencyInjection/CustomerManager.java
@@ -1,11 +1,20 @@
 package com.education.dependencyInjection;
 
 public class CustomerManager implements ICustomerService {
+
+
+
     private ICustomerDal customerDal;
     int a;
     public CustomerManager(ICustomerDal customerDal) {
         this.customerDal = customerDal;
     }
+
+    // setter injection
+    /* public void setCustomerDal(ICustomerDal customerDal){
+    this.customerDal=customerDal;
+    }
+     */
 
     public void add(){
 
--- src/main/java/com/education/dependencyInjection/MysqlCustomerDal.java
+++ src/main/java/com/education/dependencyInjection/MysqlCustomerDal.java
@@ -1,10 +1,20 @@
 package com.education.dependencyInjection;
 
 public class MysqlCustomerDal implements ICustomerDal{
+    String connectionString;
+    public String getConnectionString() {
+        return connectionString;
+    }
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
 
 
     @Override
     public void add() {
+        System.out.println("Connection String: "+this.connectionString);
+
         System.out.println("Mysql veri tabanına ekledi");
     }
 }
